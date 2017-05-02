8081a37aca1f2b21f3b670c3f40326507c688e67
mothermoose <melanie.ilseman@gmail.com>
2017-05-01 21:29:04 -0500

    initial commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,6 @@
+class ContactsController < ApplicationController
+
+  def contact_1_action
+    render contact_1.html.erb
+  end 
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+ get '/my_contacts' => 'contacts#contact_1_action'
+end
